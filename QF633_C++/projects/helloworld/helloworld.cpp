d5b446e1edcdf9c5437f22763cfd866dce98c12e
Julian Chang <CHANGJULIAN17@YAHOO.COM>
2025-04-26 10:35:23 +0800

    update with notes and one assignment 1.

--- QF633_C++/projects/helloworld/helloworld.cpp
+++ QF633_C++/projects/helloworld/helloworld.cpp
@@ -3,13 +3,17 @@
 #include <string>
 
 using namespace std;
+
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code",
-    "and the C++ extension!"};
+                        "and the C++ extension!"};
+
     for (const string& word : msg)
-        {
+    {
         cout << word << " ";
-        }
+    }
     cout << endl;
+
+    return 0;
 }
